6cc1ea745de6192acb74d13af49e3ece3fc36115
Maleesha Herath <malishaherath77@gmail.com>
2023-07-24 19:58:26 +0530

    added home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Covid 19',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
