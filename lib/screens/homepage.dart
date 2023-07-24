09b9a1a3bba0fe3d78e86245b541f8bddec73622
Maleesha Herath <malishaherath77@gmail.com>
2023-07-24 20:53:33 +0530

    s

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:covid_19/services/api_services.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../components/custom_container.dart';
@@ -10,72 +12,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ApiService service = ApiService();
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: FutureBuilder(
+      future: service.getData(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          Map<String, dynamic> data = snapshot.data!;
+          return SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
                 children: [
-                  Icon(Icons.menu),
-                  Text(
-                    "Covid 19",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w900,
-                    ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.menu),
+                      Text(
+                        "Covid 19",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Icon(
+                        Icons.add_location,
+                      )
+                    ],
                   ),
-                  Icon(
-                    Icons.add_location,
-                  )
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomContainer(
+                        size: size,
+                        title: "Loacal Deaths",
+                        color: Colors.red,
+                        value: data['local_deaths'],
+                      ),
+                      CustomContainer(
+                        size: size,
+                        title: "Total Recovered",
+                        color: Colors.green,
+                        value: data['local_recovered'],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomContainer(
+                        size: size,
+                        title: "Active Cases",
+                        color: Colors.blue,
+                        value: data['local_active_cases'],
+                      ),
+                      CustomContainer(
+                        size: size,
+                        title: "Total Cases",
+                        color: Colors.orange,
+                        value: data['local_total_cases'],
+                      ),
+                    ],
+                  ),
                 ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomContainer(
-                    size: size,
-                    title: "Loacal Deaths",
-                    color: Colors.red,
-                    value: 2256,
-                  ),
-                  CustomContainer(
-                    size: size,
-                    title: "Total Recovered",
-                    color: Colors.green,
-                    value: 1560,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomContainer(
-                    size: size,
-                    title: "Active Cases",
-                    color: Colors.blue,
-                    value: 2256,
-                  ),
-                  CustomContainer(
-                    size: size,
-                    title: "Total Cases",
-                    color: Colors.orange,
-                    value: 1560,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        }
+        return const Center(child: CupertinoActivityIndicator());
+      },
+    ));
   }
 }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,3 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
 class ApiService {
   final endPoint = "https://www.hpb.health.gov.lk/api/get-current-statistical";
+
+  Future<Map<String, dynamic>> getData() async {
+    Response response = await get(Uri.parse(endPoint));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      Map<String, dynamic> data = body['data'];
+      return data;
+    } else {
+      throw Exception(response.statusCode);
+    }
+  }
 }
