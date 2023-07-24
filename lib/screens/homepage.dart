8af8d79910231f207a0219b836475f39f8556bc8
Maleesha Herath <malishaherath77@gmail.com>
2023-07-24 20:28:27 +0530

    w

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/custom_container.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,28 +12,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.menu),
-                Text(
-                  "Covid 19",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.menu),
+                  Text(
+                    "Covid 19",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
-                ),
-                Icon(
-                  Icons.add_location,
-                )
-              ],
-            )
-          ],
+                  Icon(
+                    Icons.add_location,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomContainer(
+                    size: size,
+                    title: "Loacal Deaths",
+                    color: Colors.red,
+                    value: 2256,
+                  ),
+                  CustomContainer(
+                    size: size,
+                    title: "Total Recovered",
+                    color: Colors.green,
+                    value: 1560,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomContainer(
+                    size: size,
+                    title: "Active Cases",
+                    color: Colors.blue,
+                    value: 2256,
+                  ),
+                  CustomContainer(
+                    size: size,
+                    title: "Total Cases",
+                    color: Colors.orange,
+                    value: 1560,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
